04ac37e435cdaca345b1ef5c63b546ad9536aed5
vagrant <aileentobrien@gmail.com>
2016-10-19 21:04:19 +0000

    finished example tweet css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 var text_max = 140
 // $(".new-tweet, counter").html(text_remaining)
 
-$(".new-tweet form textarea").on("keyup", (function() {
+$(".new-tweet textarea").on("keyup", (function() {
     var text_length = $(this).val().length;
     var text_remaining = text_max - text_length;
 
-    var countdwn = $(".new-tweet form span").text(text_remaining);
+    var countdwn = $(".new-tweet span").text(text_remaining);
     if (text_remaining < 0) {
-      $(".new-tweet form span").css({"color":"red"});
+      $(".new-tweet span").css({"color":"red"});
     }
 }));
 
